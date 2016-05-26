b36c47630a0c85b474d63c9a9663f52563007b30
suchox <suchandrim.sarkar@gmail.com>
2016-05-27 01:17:14 +0530

    Initial commit

---
+++ app/src/main/java/com/sucho/camrena/PhotoActivity.java
@@ -0,0 +1,209 @@
+package com.sucho.camrena;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.hardware.Camera;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Surface;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import com.sucho.camrena.customview.CameraPreview;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class PhotoActivity extends AppCompatActivity {
+    private static final String TAG = "PhotoActivity";
+    private Camera camera;
+    private CameraPreview cameraPreview;
+    FloatingActionButton photoCapture,videoCapture,swapCamera;
+
+    int camIdx=0;
+    File imageFile;
+
+    Camera.PictureCallback captureCallback = new Camera.PictureCallback()
+    {
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+
+            int angleToRotate = getRoatationAngle(camIdx);
+
+            angleToRotate = angleToRotate + 180;
+            Bitmap orignalImage = BitmapFactory.decodeByteArray(data, 0, data.length);
+            Bitmap bitmapImage = rotate(orignalImage, angleToRotate);
+            imageFile = getImageFile();
+            if (imageFile == null) {
+                return;
+            }
+            try {
+                FileOutputStream outStream = new FileOutputStream(imageFile);
+                bitmapImage.compress(Bitmap.CompressFormat.JPEG, 100, outStream);
+                outStream.flush();
+                outStream.close();
+                MediaStore.Images.Media.insertImage(getContentResolver(),imageFile.getAbsolutePath(),imageFile.getName(),imageFile.getName());
+                camera.startPreview();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    };
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo);
+        initCamera();
+        FrameLayout cameraPreviewFrame = (FrameLayout) findViewById(R.id.camera_preview);
+        cameraPreviewFrame.addView(cameraPreview);
+
+        photoCapture = (FloatingActionButton) findViewById(R.id.photo_capture);
+        photoCapture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                camera.takePicture(null, null, captureCallback);
+            }
+        });
+
+        videoCapture = (FloatingActionButton) findViewById(R.id.video_capture);
+        videoCapture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        swapCamera = (FloatingActionButton) findViewById(R.id.swap_camera);
+        swapCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    private void initCamera()
+    {
+        if(camIdx==0) {
+            camera = getFrontCameraInstance();
+            if (camera == null) {
+                Toast.makeText(PhotoActivity.this, "No Front Camera! Switching Back Camera", Toast.LENGTH_LONG).show();
+                camera = getBackCameraInstance();
+                if (camera == null) {
+                    Toast.makeText(PhotoActivity.this, "No Camera Found", Toast.LENGTH_LONG).show();
+                    finish();
+                }
+            } else
+                cameraPreview = new CameraPreview(this, camera);
+        }
+        else if(camIdx == Camera.CameraInfo.CAMERA_FACING_FRONT)
+            camera = getFrontCameraInstance();
+        else if(camIdx == Camera.CameraInfo.CAMERA_FACING_BACK)
+            camera = getBackCameraInstance();
+    }
+
+    private Camera getFrontCameraInstance() {
+        int cameraCount = 0;
+        Camera camera = null;
+        Camera.CameraInfo cameraInfo = new Camera.CameraInfo();
+        cameraCount = Camera.getNumberOfCameras();
+        for (int camIdx = 0; camIdx < cameraCount; camIdx++) {
+            Camera.getCameraInfo(camIdx, cameraInfo);
+            if (cameraInfo.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+                try {
+                    this.camIdx = camIdx;
+                    camera = Camera.open(camIdx);
+                } catch (RuntimeException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return camera;
+    }
+
+    private Camera getBackCameraInstance() {
+        int cameraCount = 0;
+        Camera camera = null;
+        Camera.CameraInfo cameraInfo = new Camera.CameraInfo();
+        cameraCount = Camera.getNumberOfCameras();
+        for (int camIdx = 0; camIdx < cameraCount; camIdx++) {
+            Camera.getCameraInfo(camIdx, cameraInfo);
+            if (cameraInfo.facing == Camera.CameraInfo.CAMERA_FACING_BACK) {
+                try {
+                    this.camIdx = camIdx;
+                    camera = Camera.open(camIdx);
+                } catch (RuntimeException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return camera;
+    }
+    
+    private static File getImageFile() {
+        File imageStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "Camrena");
+        if (!imageStorageDir.exists()) {
+            if (!imageStorageDir.mkdirs()) {
+                Log.e(TAG, "Couldn't create Directory");
+                return null;
+            }
+        }
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File imageFile;
+        imageFile = new File(imageStorageDir.getPath() + File.separator + "PHOTO_" + timeStamp + ".jpg");
+        Log.e(TAG,imageFile.getAbsolutePath());
+        return imageFile;
+    }
+
+    public static Bitmap rotate(Bitmap bitmap, int degree) {
+        int w = bitmap.getWidth();
+        int h = bitmap.getHeight();
+
+        Matrix mtx = new Matrix();
+        mtx.postRotate(degree);
+
+        return Bitmap.createBitmap(bitmap, 0, 0, w, h, mtx, true);
+    }
+
+    public int getRoatationAngle(int cameraId) {
+        android.hardware.Camera.CameraInfo info = new android.hardware.Camera.CameraInfo();
+        android.hardware.Camera.getCameraInfo(cameraId, info);
+        int rotation = getWindowManager().getDefaultDisplay().getRotation();
+        int degrees = 0;
+        switch (rotation) {
+            case Surface.ROTATION_0:
+                degrees = 0;
+                break;
+            case Surface.ROTATION_90:
+                degrees = 90;
+                break;
+            case Surface.ROTATION_180:
+                degrees = 180;
+                break;
+            case Surface.ROTATION_270:
+                degrees = 270;
+                break;
+        }
+        int result;
+        if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+            result = (info.orientation + degrees) % 360;
+            result = (360 - result) % 360; // compensate the mirror
+        } else { // back-facing
+            result = (info.orientation - degrees + 360) % 360;
+        }
+        return result;
+    }
+}
---
+++ app/src/main/java/com/sucho/camrena/customview/CameraPreview.java
@@ -0,0 +1,111 @@
+package com.sucho.camrena.customview;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.util.Log;
+import android.view.Display;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.WindowManager;
+
+import com.sucho.camrena.PhotoActivity;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by ASUS on 26-May-16.
+ */
+public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
+{
+    private SurfaceHolder surfaceHolder;
+    private Camera camera;
+    Context context;
+    
+    public CameraPreview(Context context, Camera camera) 
+    {
+        super(context);
+        this.context = context;
+        this.camera = camera;
+        this.surfaceHolder = this.getHolder();
+        this.surfaceHolder.addCallback(this);
+    }
+    
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder) 
+    {
+        try {
+            camera.setPreviewDisplay(surfaceHolder);
+            camera.startPreview();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) 
+    {
+        camera.stopPreview();
+        camera.release();
+    }
+    
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int format, int width, int height) 
+    {
+        camera.stopPreview();
+
+        Camera.Parameters parameters = camera.getParameters();
+        WindowManager wm = (WindowManager)(context).getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+
+        Camera.Size imageSize = getImageSize(width, height, parameters);
+
+        if(display.getRotation() == Surface.ROTATION_0)
+        {
+            parameters.setPictureSize(imageSize.width, imageSize.height);
+            camera.setDisplayOrientation(90);
+        }
+
+        if(display.getRotation() == Surface.ROTATION_90)
+        {
+            parameters.setPictureSize(imageSize.width, imageSize.height);
+        }
+
+        if(display.getRotation() == Surface.ROTATION_180)
+        {
+            parameters.setPictureSize(imageSize.width, imageSize.height);
+        }
+
+        if(display.getRotation() == Surface.ROTATION_270)
+        {
+            parameters.setPictureSize(imageSize.width, imageSize.height);
+            camera.setDisplayOrientation(180);
+        }
+
+        camera.setParameters(parameters);
+
+        try {
+            camera.setPreviewDisplay(surfaceHolder);
+            camera.startPreview();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Camera.Size getImageSize(int width, int height, Camera.Parameters parameters){
+        Camera.Size bestSize = null;
+        List<Camera.Size> sizeList = parameters.getSupportedPreviewSizes();
+
+        bestSize = sizeList.get(0);
+
+        for(int i = 1; i < sizeList.size(); i++){
+            if((sizeList.get(i).width * sizeList.get(i).height) >
+                    (bestSize.width * bestSize.height)){
+                bestSize = sizeList.get(i);
+            }
+        }
+
+        return bestSize;
+    }
+}
